010554ce68dd7b2a1004164ddbf1423600ca0491
abish <abish@intersmart.in>
2026-07-30 15:49:02 +0530

    home news

--- src/components/sections/home/news.jsx
+++ src/components/sections/home/news.jsx
@@ -47,11 +47,11 @@ const local_data = {
 };
 
 export default function News({ data = local_data }) {
+  if (!data?.news_list) data = local_data;
   const [emblaRef, emblaApi] = useEmblaCarousel({
     containScroll: "trimSnaps",
     skipSnaps: false,
   });
-
   const scrollPrev = useCallback(
     () => emblaApi && emblaApi.scrollPrev(),
     [emblaApi],
@@ -60,19 +60,18 @@ export default function News({ data = local_data }) {
     () => emblaApi && emblaApi.scrollNext(),
     [emblaApi],
   );
-
   return (
     <section className="w-full h-auto py-[80px] bg-white block">
       <div className="container xl:max-w-[1160px] 2xl:max-w-[1725px] mx-auto">
-        <div className="w-full h-auto flex items-center flex-wrap">
+        <div className="w-full h-auto mb-[30px] flex items-center flex-wrap justify-between">
           <div className="text-[40px] leading-normal font-bold text-[#212121]">
             {data?.title}
           </div>
-          <div className="gap-[5px] flex justify-end">
+          <div className="[--size:30px] gap-[5px] flex justify-end">
             <button
               type="button"
               onClick={scrollPrev}
-              className="inline-flex h-11 w-11 items-center justify-center rounded-full border border-[#E2E2E2] text-[#212121] transition hover:bg-[#F4F4F4]"
+              className="w-(--size) h-(--size) p-[5px_10px] border border-[#D9D9D9] flex items-center justify-center"
               aria-label="Previous slide"
             >
               <Image
@@ -86,7 +85,7 @@ export default function News({ data = local_data }) {
             <button
               type="button"
               onClick={scrollNext}
-              className="inline-flex h-11 w-11 items-center justify-center rounded-full border border-[#E2E2E2] text-[#212121] transition hover:bg-[#F4F4F4]"
+              className="w-(--size) h-(--size) p-[5px_10px] border border-[#D9D9D9] flex items-center justify-center"
               aria-label="Next slide"
             >
               <Image
@@ -100,14 +99,14 @@ export default function News({ data = local_data }) {
           </div>
         </div>
         <div className="w-full h-auto overflow-hidden" ref={emblaRef}>
-          <div className="flex gap-6 touch-pan-y touch-pinch-zoom">
+          <div className="[--slide--sapcing:20px] ml-[calc(var(--slide--sapcing)*-1)] flex touch-pan-y touch-pinch-zoom">
             {data?.news_list?.map((item) => (
               <div
                 key={item?.id}
-                className="min-w-[300px] flex-[0_0_100%] md:flex-[0_0_50%] lg:flex-[0_0_33.333%]"
+                className="pl-(--slide--sapcing) min-w-0 flex-[0_0_100%] md:flex-[0_0_50%] lg:flex-[0_0_calc(100%/3)]"
               >
-                <div className="w-full h-full block">
-                  <div className="w-full h-auto aspect-[560/220] mb-[70px] rounded-[10px] overflow-hidden block">
+                <div className="w-full h-full bg-linear-to-b from-[#FFF8EE]/50 to-[#FFF3E0]/50 rounded-[10px] block">
+                  <div className="w-full h-auto aspect-[560/220] mb-[50px] block relative z-0">
                     <Image
                       src={item?.path}
                       width={560}
@@ -115,7 +114,7 @@ export default function News({ data = local_data }) {
                       alt={item?.title}
                       className="w-full h-full object-cover"
                     />
-                    <div className="w-auto h-auto p-[20px_10px] bg-linear-to-r from-[#DC2626] to-[#F97316] rounded-[10px] overflow-hidden flex items-center justify-center">
+                    <div className="w-auto h-auto p-[15px_10px] bg-linear-to-r from-[#DC2626] to-[#F97316] mx-[30px] rounded-[10px] overflow-hidden flex flex-col items-center justify-center absolute z-1 inset-[auto_auto_-20%_0]">
                       <span className="text-[28px] leading-normal font-semibold text-white">
                         {item?.date}
                       </span>
@@ -124,11 +123,15 @@ export default function News({ data = local_data }) {
                       </span>
                     </div>
                   </div>
-                  <div className="w-full h-auto flex justify-between">
-                    <div className="text-[23px] leading-normal font-bold text-[#212121]">
+                  <div className="w-full h-auto p-[20px_30px] flex flex-col justify-between">
+                    <div className="text-[23px] leading-normal font-bold text-[#212121] w-[90%] mb-[30px]">
                       {item?.title}
                     </div>
-                    <Link href={item?.link} target="_blank" className="">
+                    <Link
+                      href={item?.link}
+                      target="_blank"
+                      className="text-[15px] leading-normal font-bold uppercase bg-gradient-to-r from-[#DC2626] to-[#F97316] bg-clip-text text-transparent"
+                    >
                       Read More {" > "}
                     </Link>
                   </div>
