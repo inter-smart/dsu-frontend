000a17c80df0f2bb311363b47edd234b164d03a7
vishnu shaji <vishnushaji@intersmart.in>
2026-09-10 17:49:14 +0530

    sdgm page

--- src/components/sections/SDG/SdgInroduction.jsx
+++ src/components/sections/SDG/SdgInroduction.jsx
@@ -18,14 +18,14 @@ export default function SdgIntroduction({ data }) {
                         </div>
                     </div>
                     <div className="w-full lg:w-1/4">
-                        <div className="w-full max-w-[90%] mx-auto flex items-center justify-center">
+                        <div className="w-full sm:max-w-[50%] lg:max-w-[90%] mx-auto flex items-center justify-center">
                             <Image src={data?.media.url} className="object-cover w-full h-full" width={530} height={430} alt={data?.media.alternativeText} />
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-wrap -m-[4px] md:-m-[5px] lg:-m-[7px] xl:-m-[10px] 3xl:-m-[15px]">
+                <div className="flex flex-wrap lg:-m-[4px] md:-m-[5px] lg:-m-[7px] xl:-m-[10px] 3xl:-m-[15px] w-full">
                     {data?.goals.map((item, id) => (
-                        <div className="w-1/3 sm:w-1/4 lg:w-1/5 p-[4px] md:m-[5px] lg:p-[7px] xl:p-[10px] 3xl:p-[15px]" key={id}>
+                        <div className="w-1/3 sm:w-1/4 lg:w-1/5 p-[4px] md:p-[5px] lg:p-[7px] xl:p-[10px] 3xl:p-[15px]" key={id}>
                             <div className="w-full h-full relative">
                                 <Image src={item.image.url} className="object-cover w-full h-full" width={530} height={430} alt={item?.image.alternativeText} />
                             </div>
